703e5964e95a4cc8e9fc4990f15fcf0c4afc3318
smarsh25 <sjm.atl@gmail.com>
2014-02-12 21:13:01 -0800

    adding validation for blank fields in comment, prevent.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 
 	# accept the associate comments, as long as they are not blank
-	accepts_nested_attributes_for :comments
+	accepts_nested_attributes_for :comments, reject_if: proc { |attributes| attributes['body'].blank? }
 end
